b87e950794ab6165b19e4e141caf018ff21208fd
miladkth <116656210+miladkth@users.noreply.github.com>
2024-01-12 13:49:29 +0100

    msg service kontrollerad ->ok

--- back/src/main/java/kth/milad/Lab1Application.java
+++ back/src/main/java/kth/milad/Lab1Application.java
@@ -1,19 +1,7 @@
 package kth.milad;
 
-import kth.milad.controller.PatientController;
-import kth.milad.entity.Doctor;
-import kth.milad.entity.Encounter;
-import kth.milad.entity.Msg;
-import kth.milad.entity.Patient;
-import kth.milad.repository.PatientRepository;
-import kth.milad.service.IService;
-import kth.milad.service.PatientServiceImp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 public class Lab1Application {
